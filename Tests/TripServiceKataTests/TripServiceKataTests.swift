15a106052086a11ec557781c4f007ea450436a79
Ahmed ibrahim <ahmed-ibrahim-salim>
2024-03-06 00:19:52 +0200

    3 tests not pass

--- Tests/TripServiceKataTests/TripServiceKataTests.swift
+++ Tests/TripServiceKataTests/TripServiceKataTests.swift
@@ -21,28 +21,80 @@ class TripServiceKataTests: XCTestCase {
     
     func test_whenUserIsLoggedIn_DoesNotThrowError() throws {
         let user = User()
-        let userSession = UserSession()
-        tripService?.userSession = UserSessionMock()
-        XCTAssertNoThrow(try tripService?.getTripsByUser(user), "no message")
+
+        XCTAssertNoThrow(
+            try prepareTripServiceWhenUserIsLoggedIn()?.getTripsByUser(user),
+            "no message"
+        )
+    }
+    
+    func test_AddingAFriend_WhichIsNotTheSameUser_tripsIsNil() throws {
+        let user = User()
+        let friend1 = User()
+        user.addFriend(friend1)
+        
+        let trips = try prepareTripServiceWhenUserIsLoggedIn()?.getTripsByUser(user)
+        
+        XCTAssertEqual(trips?.count, nil)
+    }
+    
+    func test_AddingAFriend_WhichIsTheSameUser_tripsIsNil() throws {
+        let user = User()
+        user.addFriend(user)
+        let tripService = prepareTripServiceWhenUserIsLoggedIn()
+        
+        guard let userSessionMock = tripService?.userSession as? UserSessionMock else {
+            XCTFail()
+            return
+        }
+        
+        userSessionMock.user = user
+        
+        tripService?.tripDAO = TripDAOMock()
+        let trips = try tripService?.getTripsByUser(user)
+//
+        XCTAssertEqual(trips?.count, 1)
     }
     
 }
 
-class UserSessionMock: UserSession {
+extension TripServiceKataTests {
+    func prepareTripServiceWhenUserIsLoggedIn() -> TestableTripService?{
+        tripService?.userSession = UserSessionMock()
+        return tripService
+    }
+}
 
+class TripDAOMock: TripDAO {
+    override class func findTripsByUser(_ user: User) throws -> [Trip]? {
+        user.addTrip(Trip())
+        return user.trips()
+    }
+}
+
+class UserSessionMock: UserSession {
+    weak var user: User?
     override func getLoggedUser() throws -> User? {
-        User()
+        user
+    }
+    override func isUserLoggedIn(_ user: User) throws -> Bool {
+        self.user == user
     }
 }
 
 class TestableTripService: TripService {
-    var userSession: UserSession?
-    
+    weak var userSession: UserSession?
+    weak var tripDAO: TripDAO?
+
     override func getLoggedUser() throws -> User? {
         try! userSession?.getLoggedUser()
     }
     
     override func findTripsByUser(_ user: User) throws -> [Trip]? {
-        nil
+        guard tripDAO is TripDAOMock else {
+            return try! TripDAO.findTripsByUser(user)
+        }
+        return try! TripDAOMock.findTripsByUser(user)
+        
     }
 }
